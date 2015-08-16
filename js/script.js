5222786209cbd8cc7c84144d1b15ae15f38ac5d8
hungnm11 <hung.nguyen@hotmail.com.vn>
2015-08-16 16:34:55 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -1,45 +1,8 @@
-(function(){
-
-    var $images = ['tech1_1000x300.jpg','tech2_1000x300.jpg','tech3_1000x300.jpg'];
+(function() {
     
-    $('#nav').affix({
-        offset: {
-            top: $('#nav').offset().top
-        }
-    });
-    
-    $(window).resize(function(e) {
-        var div = $('#banner');
-        var size = div.height()/3;
-        $('#bannerthumb li img').css({
-            height: size
-        });
-    });
-    
-    var size = $('#banner').height()/3;
-    $('#bannerthumb li img').css({
-            height: size
-    });
-    
-    $('#bannerthumb li').click(function(e) {
+    $(".myBtn").click(function(e) {
         e.preventDefault();
-        var img = 'img/' + $images[$(this).index()];
-        console.log(img);
-        $('.main-photo').hide().fadeIn('slow').attr('src',img);
-        
-        
+        $("#myModal").modal();
     });
     
-    var count = 1;
-    function autoplay() {
-        var img = 'img/' + $images[count];
-        if ($images.length > count) {
-            $('.main-photo').hide().fadeIn('slow').attr('src',img);
-            count++;
-        }  else {
-            count = 0;
-        }
-        setTimeout(autoplay, 5000);
-    }
-    setTimeout(autoplay, 5000);
 })();
